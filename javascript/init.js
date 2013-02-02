60b8e0ca0ea7855469d07fd0ab6673379c4b0c62
Kendell Joseph <contact@kendelljoseph.com>
2013-02-01 22:43:42 -0500

    Pick a font dude.

--- javascript/init.js
+++ javascript/init.js
@@ -1,11 +1,19 @@
+
 $(function () {
     var app = $('body');
     var client = {};
     
-    client.welcomeMessage = (function(){
+    client.DELETEME = (function(){
        var div = $('<div />')
-            .attr('id')
-        
+            .attr('id','supbro')
+            .html('This font makes comic sans look good. Find a better one, or this is going to be the default. ')
+            .append(
+                $('<a />')
+                    .html('How do you know this wont take you to Live Jasmine?')
+                    .attr('href', 'http://www.google.com/webfonts')
+            )
+            .appendTo(app);
        return div;
-    });
+    })();
+    
 });		
