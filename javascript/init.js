93c2cde9b989239524592153662b09fd16fbabb8
Kendell Joseph <contact@kendelljoseph.com>
2013-02-01 22:29:20 -0500

    Still setting up

---
+++ javascript/init.js
@@ -0,0 +1,11 @@
+$(function () {
+    var app = $('body');
+    var client = {};
+    
+    client.welcomeMessage = (function(){
+       var div = $('<div />')
+            .attr('id')
+        
+       return div;
+    });
+});		
