4f7a902a03966401d0b9f8e9347ecbf15e224a8a
Kendell Joseph <contact@kendelljoseph.com>
2013-02-03 13:55:32 -0500

    Trying not to ruin vins life.

--- javascript/init.js
+++ javascript/init.js
@@ -2,7 +2,7 @@
 var imagename = "sitesignature.png";
 var imageSettings = {
     height: "86px",
-    width: "275px"
+    width: "274px"
 };
 
 $(function () {
@@ -17,10 +17,13 @@ $(function () {
             .appendTo(app);
        return div;
     })();
+    
     centerElement(client.splashImage);
+    $(window).resize(function(){
+       centerElement(client.splashImage);
+    });
     
     function centerElement(element) {
-        console.log(element.width(),$(document).width())
         var x = $(document).width() / 2 - (element.width()/2) - (parseInt(element.css("padding"), 10) / 2);
         var y = $(document).height() / 2 - (element.height()/2) - (parseInt(element.css("padding"), 10) / 2);
         element.css({
